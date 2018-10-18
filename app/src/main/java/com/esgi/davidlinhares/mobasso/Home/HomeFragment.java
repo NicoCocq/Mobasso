cabcc6876a916d56c7b5faf9e6e08371c19d9098
Linhares David <linhares.david@digipolitan.com>
2018-10-18 13:07:00 +0200

    Added update mode / Added super user system / added login ui

--- app/src/main/java/com/esgi/davidlinhares/mobasso/Home/HomeFragment.java
+++ app/src/main/java/com/esgi/davidlinhares/mobasso/Home/HomeFragment.java
@@ -1,17 +1,28 @@
 package com.esgi.davidlinhares.mobasso.Home;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.esgi.davidlinhares.mobasso.MainActivity;
 import com.esgi.davidlinhares.mobasso.R;
+import com.esgi.davidlinhares.mobasso.login.LoginActivity;
 
+import java.util.TimerTask;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.esgi.davidlinhares.mobasso.MainActivity.STATIC_INTEGER_VALUE;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,6 +33,11 @@ import butterknife.ButterKnife;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+
+    @BindView(R.id.home_association_logo)
+    ImageView logo;
+
+    private short touches;
 
     @Nullable
     @Override
@@ -38,5 +54,39 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+    }
+
+    @OnClick(R.id.home_association_logo)
+    void onTouchAssociationLogo() {
+        final CountDownTimer timer = new CountDownTimer(3000, 1) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                timerFinished();
+            }
+        };
+
+        if(touches == 0) {
+           timer.start();
+        }
+
+        touches++;
+
+        if(touches == 3) {
+            if(getActivity() instanceof MainActivity) {
+                timer.onFinish();
+                startActivityForResult(new Intent(this.getActivity(), LoginActivity.class), STATIC_INTEGER_VALUE);
+                logo.setClickable(false);
+                touches = 0;
+            }
+        }
+    }
+
+    private void timerFinished() {
+        touches = 0;
     }
 }
--- app/src/main/java/com/esgi/davidlinhares/mobasso/MainActivity.java
+++ app/src/main/java/com/esgi/davidlinhares/mobasso/MainActivity.java
@@ -1,5 +1,9 @@
 package com.esgi.davidlinhares.mobasso;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -7,6 +11,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.esgi.davidlinhares.mobasso.Donation.DonationFragment;
@@ -15,11 +21,19 @@ import com.esgi.davidlinhares.mobasso.News.NewsFragment;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int STATIC_INTEGER_VALUE = 200;
     @BindView(R.id.bottomNavigationView) public BottomNavigationView bottomNavigationView;
+    @BindView(R.id.main_save) public Button saveButton;
     private Fragment fragment;
+    private boolean isSuperUserActivated = false;
+
+    public boolean isSuperUserActivated() {
+        return isSuperUserActivated;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,4 +64,35 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void setSuperUserActivated(boolean activated) {
+
+        SharedPreferences sharedPref = getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putBoolean(getString(R.string.SUPER_USER_ACTIVATED), activated);
+        editor.apply();
+        editor.commit();
+
+        this.isSuperUserActivated = activated;
+        if(isSuperUserActivated) {
+            saveButton.setVisibility(View.VISIBLE);
+            return;
+        }
+
+        saveButton.setVisibility(View.GONE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        SharedPreferences sharedPref = getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        boolean activated = sharedPref.getBoolean(getString(R.string.SUPER_USER_ACTIVATED), false);
+        this.setSuperUserActivated(activated);
+    }
+
+    @OnClick(R.id.main_save)
+    void onSaveClicked() {
+        this.setSuperUserActivated(false);
+    }
 }
